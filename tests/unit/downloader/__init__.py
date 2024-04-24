33da0dcdb5c792c60d9a185c6ccf725703fcd358
lsalmela <lsalmela@users.noreply.github.com>
2024-04-24 18:13:34 +0000

    deploy: 0f12260b8976a950b35a44cc1723f59e23ee0fbe

--- tests/unit/downloader/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Package defining downloader subpackage unit tests."""
-
-# SPDX-FileCopyrightText: 2023 Lauri Salmela <lauri.m.salmela@gmail.com>
-#
-# SPDX-License-Identifier: MIT
--- xbrl_filings_api/__about__.py
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Lauri Salmela <lauri.m.salmela@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-
-__version__ = '1.0.0.dev1'
